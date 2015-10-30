c76e648f065b3d185915e3dd6778ee733fbcd94d
suribabu <suribabu.rss@gmail.com>
2015-10-30 16:03:56 +0530

    sql commentts added

--- testgitvs/Controllers/OffersApiController.cs
+++ testgitvs/Controllers/OffersApiController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -20,8 +22,8 @@ namespace testgitvs.Controllers
             of.offerID = 1;
             of.Available_On = 'O';
             of.Coupen_Code = "OCT50";
-            of.Offer_Image1_Path = "a";
-            of.Offer_image2_Path = "a";
+            of.Offer_Image1_Path = "http://placehold.it/50x50&text=img1";
+            of.Offer_image2_Path = "http://placehold.it/120x120&text=img1"; ;
             of.Offer_Title = "10% off with sbi credit";
             of.Offer_Link = "d";
             of.moreDescription = "recharge your mobile with paytm and get 50% cash back";
@@ -30,13 +32,47 @@ namespace testgitvs.Controllers
             of2.offerID = 1;
             of2.Available_On = 'S';
             of2.Coupen_Code = "OCT10";
-            of2.Offer_Image1_Path = "paytmpath";
-            of2.Offer_image2_Path = "a";
+            of2.Offer_Image1_Path = "http://placehold.it/50x50&text=img2";
+            of2.Offer_image2_Path = "http://placehold.it/120x120&text=img2";
             of2.Offer_Title = "50% off on offer";
             of2.Offer_Link = "www.google.com";
             of2.moreDescription = "book  your bus ticket with redbus and get 20% cash back";
             ofr.Add(of2);
             return ofr;
+
+
+
+            //SqlConnection conn = null;
+            //SqlDataReader rdr = null;
+
+            //Console.WriteLine("\nTop 10 Most Expensive Products:\n");
+
+            //try
+            //{
+            //    conn = new SqlConnection("Server=(local);DataBase=master;Integrated Security=SSPI");
+            //    conn.Open();
+            //    SqlCommand cmd = new SqlCommand("dbo.test", conn);
+            //    cmd.CommandType = CommandType.StoredProcedure;
+            //    rdr = cmd.ExecuteReader();
+            //    /*while (rdr.Read())
+            //    {
+            //        Console.WriteLine(
+            //            "Product: {0,-25} Price: ${1,6:####.00}",
+            //            rdr["TenMostExpensiveProducts"],
+            //            rdr["UnitPrice"]);
+            //    }*/
+            //}
+            //finally
+            //{
+            //    if (conn != null)
+            //    {
+            //        conn.Close();
+            //    }
+            //    if (rdr != null)
+            //    {
+            //        rdr.Close();
+            //    }
+            //}
         }
     }
 }
